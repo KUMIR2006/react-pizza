f3366eabfe5541f082d860052b88d1fac8c4ca88
KUMIR2006 <kurbanovmirzobek@gmail.com>
2025-02-17 02:32:58 +0300

    List rendering

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
+import pizzas from './assets/pizzas.json';
+
 import './scss/app.scss';
 
 function App() {
@@ -20,10 +22,9 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              <PizzaBlock title="Мексиканская" price={358} />
-              <PizzaBlock title="Пепперони" price={459} />
-              <PizzaBlock title="Ливанская" price={124} />
-              <PizzaBlock title="Афганская" price={1200} />
+              {pizzas.map((obj) => (
+                <PizzaBlock {...obj} />
+              ))}
             </div>
           </div>
         </div>
