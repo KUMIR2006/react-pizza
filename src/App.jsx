107487143b21b96e2b3d934b6d414f407179a5e0
KUMIR2006 <kurbanovmirzobek@gmail.com>
2025-02-18 01:07:17 +0300

    Fetch data from the backend

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,33 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-
-import pizzas from './assets/pizzas.json';
+import axios from 'axios';
 
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://67b3a70d392f4aa94fa7e9b9.mockapi.io/items')
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
+
+//   React.useEffect(() =>
+//     {
+//       async function fetchData() {
+//         try {
+//           axios.get('https://67b3a70d392f4aa94fa7e9b9.mockapi.io/items')
+//           .then((data) => {setItems(data)});
+//         }
+//         catch (error) {alert("Ошибка при запросе данных :(")}
+//       }
+//       fetchData();
+// }, []);
   return (
     <div>
       <div className="wrapper">
@@ -22,7 +43,7 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map((obj) => (
+              {items.map((obj) => (
                 <PizzaBlock key={obj.id} {...obj} />
               ))}
             </div>
