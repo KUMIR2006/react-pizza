08a043c84d2ea301c2df4515af170e9e5b54c738
KUMIR2006 <kurbanovmirzobek@gmail.com>
2025-02-19 00:07:15 +0300

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,30 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
-import axios from 'axios';
+
+import Home from './pages/Home';
+import Cart from './pages/Cart'
+import NotFound from './pages/NotFound'
+
+
 
 import './scss/app.scss';
 
 function App() {
-  const [items, setItems] = React.useState([]);
 
-  React.useEffect(() => {
-    fetch('https://67b3a70d392f4aa94fa7e9b9.mockapi.io/items')
-      .then((res) => res.json())
-      .then((json) => {
-        setItems(json);
-      });
-  }, []);
-
-
-//   React.useEffect(() =>
-//     {
-//       async function fetchData() {
-//         try {
-//           axios.get('https://67b3a70d392f4aa94fa7e9b9.mockapi.io/items')
-//           .then((data) => {setItems(data)});
-//         }
-//         catch (error) {alert("Ошибка при запросе данных :(")}
-//       }
-//       fetchData();
-// }, []);
   return (
     <div>
       <div className="wrapper">
         <Header />
         <div className="content">
           <div className="container">
-            <div className="content__top">
-              <Categories />
-              <Sort />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-              {items.map((obj) => (
-                <PizzaBlock key={obj.id} {...obj} />
-              ))}
-            </div>
+            <Routes>
+              <Route path="/" exact element={<Home/> } />
+              <Route path="/cart" exact element={<Cart/> } />
+              <Route path="*" exact element={<NotFound/>} />
+            </Routes>
+
           </div>
         </div>
       </div>
