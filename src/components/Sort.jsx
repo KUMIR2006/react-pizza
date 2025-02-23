3aeb94d542b901cb6ec2774525d7c0963b8b2666
KUMIR2006 <kurbanovmirzobek@gmail.com>
2025-02-24 00:31:22 +0300

    Saving the filtering parameters

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setSort } from "../redux/slices/filterSlice";
 
-const list = [
+export const sortList = [
   {name: "популярности ⬇", sortProperty: "rating"}, 
   {name: "популярности ⬆", sortProperty: "-rating"},
   {name: "цене ⬇", sortProperty: "price"}, 
@@ -46,7 +46,7 @@ function Sort() {
           <div className="sort__popup">
             <ul>
                 {          
-                  list.map((obj, i) => 
+                  sortList.map((obj, i) => 
                     <li 
                       key={i}
                       onClick={() => onClickListItem(obj)} 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ import Pagination from '../components/Pagination';
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
+import {sortList} from '../components/Sort';
 
 
 import { setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
@@ -17,6 +18,8 @@ import { SearchContext } from '../App';
 function Home() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
+  const isSearch = React.useRef(false)
+  const isMounted = React.useRef(false)
 
   const {categoryId, sort, currentPage } = useSelector(state => state.filter)
   const sortType = sort.sortProperty;
@@ -37,24 +40,7 @@ function Home() {
     dispatch(setCurrentPage(number));
   }
 
-
-
-  React.useEffect(() => {
-    if (window.location.search){
-      const params = qs.parse(window.location.search.substring(1));
-
-      const sort = sortList.find(obj => obj.sortProperty === params.sortProperty)
-
-      dispatch(
-        setFilters({
-          ...params,
-          sort,
-        })
-      );
-    }
-  }, []);
-
-  React.useEffect(() => {
+  const fetchPizzas = () => {
     setIsLoading(true);
 
     const sortBy = sortType.replace('-', '')
@@ -68,20 +54,51 @@ function Home() {
           setItems(res.data);
           setIsLoading(false)
         })
+  }
 
-      window.scrollTo(0,0);
-  }, [categoryId, sortType, searchValue, currentPage]);
-  
+  // Если был первый рендер, то проверяем URL-параметры и сохраняем в редуксе
   React.useEffect(() => {
-    const queryString = qs.stringify({
-      sortProperty: sort.sortProperty,
-      categoryId,
-      currentPage,
-    });
+    if (window.location.search){
+      const params = qs.parse(window.location.search.substring(1));
 
-    navigate(`?${queryString}`)
+      const sort = sortList.find(obj => obj.sortProperty === params.sortProperty)
+
+      dispatch(
+        setFilters({
+          ...params,
+          sort,
+        })
+      );
+      isSearch.current = true;
+    }
+  }, []);
+
+  // Если изменили параметры и был первый рендер
+  React.useEffect(() => {
+    if(isMounted.current){
+      const queryString = qs.stringify({
+        sortProperty: sortType,
+        categoryId,
+        currentPage,
+      });
+
+      navigate(`?${queryString}`);
+    }
+    isMounted.current = true;
+    
   }, [categoryId, sortType, currentPage]);
   
+  // Если был первый рендер, то запрашиваем пиццы
+  React.useEffect(() => {
+    window.scrollTo(0,0);
+
+    if(!isSearch.current){
+      fetchPizzas();
+    }
+    isSearch.current = false;
+  }, [categoryId, sortType, searchValue, currentPage]);
+  
+
   return(
     
     <div className="container">
